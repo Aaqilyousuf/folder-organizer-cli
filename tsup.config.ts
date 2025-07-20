f5428a39bd493b970e56f2283fb6f9e5dc189112
Aaqilyousuf <aaqilyousuf@gmail.com>
2025-07-20 17:46:01 +0530

    WIP: changes before version bump

--- tsup.config.ts
+++ tsup.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "tsup";
 
 export default defineConfig({
   entry: ["src/index.ts"],
-  format: ["cjs", "esm"],
+  format: "cjs",
   outDir: "dist",
   target: "node18",
   dts: true, // Generate declaration files
